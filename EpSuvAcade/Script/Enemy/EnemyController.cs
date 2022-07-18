88f704de1e94456207f08a4f54e7c9f66618f012
Aueld <epeld430@gmail.com>
2022-07-18 09:36:00 +0900

    Unity C#

--- EpSuvAcade/Script/Enemy/EnemyController.cs
+++ EpSuvAcade/Script/Enemy/EnemyController.cs
@@ -10,29 +10,30 @@ public class EnemyController : MonoBehaviour
 
     [SerializeField] private EnemyData enemyData;
 
-    private Player playerLogic;
-    private Rigidbody2D rb;
-    private SpriteRenderer spriteRenderer;
+    private Player playerLogic;         // 플레이어 스크립트
+    private Rigidbody2D rb;             // 물리값
+    private SpriteRenderer spriteRenderer; // 스프라이트 이미지
 
     private float playerHitTime = 0.2f; // 적 한명에게 맞고 그 다음 맞을 때 시간
-    private float curHp;
-    private float BigBulletHitTime;
+    private float curHp;                // 변경된 체력
+    private float BigBulletHitTime;     // 장판 피격 딜레이
 
-    public GameManager gm;
-    public Text DestroyEnemyCount;
+    public GameManager gm;              // 게임매니저
+    public Text DestroyEnemyCount;      // 처치된 몬스터
     public GameObject player;
     public GameObject canvas;
     public Image hpBar;
 
     private bool isHit;
+
     private void Awake()
     {
         rb = GetComponentInChildren<Rigidbody2D>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
-    // 오브젝트 풀에 의해 다시 활성화되면 다 초기화 해줘야 함
-    private void OnEnable()    
+    // 오브젝트 풀에 의해 다시 활성화되면 초기화
+    private void OnEnable()   
     {
         curHp = enemyData.Hp;
         hpBar.fillAmount = (float)curHp / enemyData.Hp;
@@ -41,13 +42,15 @@ public class EnemyController : MonoBehaviour
     
     private void Update()
     {
-        if(!isHit)
+        if(!isHit) // 피격시 잠시 정지
             rb.velocity = Vector2.zero;
+
         Move();
     }
 
     private void Move()
     {
+        // 플레이어에게로 이동
         Vector3 movement = (player.transform.position - transform.position).normalized;
         transform.Translate(movement * enemyData.MoveSpeed * Time.deltaTime);
 
@@ -64,7 +67,7 @@ public class EnemyController : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.tag == "ability Multi Hit")
+        if (collision.tag == "ability Multi Hit")   // 멀티 다단 히트
         {
             playerLogic = player.GetComponent<Player>();
             float abilityDamage = collision.GetComponent<Ability>().damage + playerLogic.playerDamage;
@@ -76,7 +79,7 @@ public class EnemyController : MonoBehaviour
             Vector2 reactVec = new Vector2(transform.position.x - collision.transform.position.x,
             transform.position.y - collision.transform.position.y).normalized;
 
-            BigBulletHitTime += Time.deltaTime;
+            BigBulletHitTime += Time.deltaTime;                 // 다단히트 딜레이
             if (BigBulletHitTime > 0.5f)
             {
                 try
@@ -92,9 +95,9 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)     // 적 히트
     {
-        if (collision.tag == "ability")
+        if (collision.tag == "ability") // 능력에 피격
         {
             playerLogic = player.GetComponent<Player>();
             float abilityDamage = collision.GetComponent<Ability>().damage + playerLogic.playerDamage;
@@ -117,12 +120,12 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)   // 플레이어 피격
     {
         if(collision.gameObject.tag == "character")
         {
             playerHitTime += Time.deltaTime;
-            if (playerHitTime >= 0.2f)
+            if (playerHitTime >= 0.2f)      // 피격 딜레이
             {
                 playerHitTime = 0.0f;
                 playerLogic = player.GetComponent<Player>();
@@ -131,28 +134,29 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    private IEnumerator HideHp()
+    private IEnumerator HideHp()    // 10초 동안 피격 당하지 않을시 체력바 숨김
     {
         yield return new WaitForSeconds(10.0f);
 
         canvas.SetActive(false);
     }
 
-    private IEnumerator Hit(Vector2 reactVec, float damage)
+    private IEnumerator Hit(Vector2 reactVec, float damage) // 피격
     {
         curHp -= damage;
 
 
-        GameObject dmgTextObj = ObjectPool.Instance.GetObject("DmgText");
+        GameObject dmgTextObj = ObjectPool.Instance.GetObject("DmgText");       // 데미지 텍스트
         dmgTextObj.transform.position = transform.position + Vector3.up * 3f;
         dmgTextObj.SetActive(true);
+
         TextMeshPro dmgTextObjText = dmgTextObj.GetComponent<TextMeshPro>();
         dmgTextObjText.text = ((int)damage).ToString();
 
 
         hpBar.fillAmount = (float)curHp / enemyData.Hp;
 
-        if (curHp > 0)
+        if (curHp > 0)  // 체력 0 초과일때
         {
             isHit = true;
             rb.AddForce(reactVec * 5f, ForceMode2D.Impulse);
@@ -162,18 +166,25 @@ public class EnemyController : MonoBehaviour
             isHit = false;
 
         }
-        else
+        else            // 체력 0 이하일때
         {
-            var obj = ObjectPool.Instance.GetObject("RedSoul");
-            obj.transform.position = gameObject.transform.position;
-            obj.SetActive(true);
+            int ran = Random.Range(0, 10);
 
-            if (transform.gameObject.name == "Monster_Boss1")
+            var obj = ObjectPool.Instance.GetObject("RedSoul");
+
+            if (ran > 1)    // 80% 확률로 드랍
             {
-                for (int i = 0; i < 5; i++)
+                obj.transform.position = gameObject.transform.position;
+                obj.SetActive(true);
+            }
+
+            if (transform.gameObject.name == "Monster_Boss(Clone)")   // 보스 처치시 상위 아이템 드랍
+            {
+                for (int i = 0; i < 20; i++)
                 {
-                    obj = ObjectPool.Instance.GetObject("RedSoul");
-                    obj.transform.position = gameObject.transform.position;
+                    obj = ObjectPool.Instance.GetObject("BlueSoul");
+                    obj.transform.position = gameObject.transform.position
+                         + (Vector3) (Random.insideUnitCircle + new Vector2(Random.Range(0.02f, 0.05f), Random.Range(0.02f, 0.05f)));
                     obj.SetActive(true);
                 }
             }
@@ -184,6 +195,5 @@ public class EnemyController : MonoBehaviour
             spriteRenderer.color = Color.white;
             yield break;
         }
-
     }
 }
