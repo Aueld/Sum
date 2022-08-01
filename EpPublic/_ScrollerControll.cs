cb358d200c2414fc3796a011d0e7d10b953f76ef
Aueld <epeld430@gmail.com>
2022-08-01 11:04:10 +0900

    Unity C#

--- EpPublic/_ScrollerControll.cs
+++ EpPublic/_ScrollerControll.cs
@@ -2,27 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class _ScrollerControll : MonoBehaviour
 {
     public ScrollRect scrollRect;
-
-    public float space = 30f;
     public GameObject uiPrefabs;
     public List<RectTransform> uiObjects = new List<RectTransform>();
 
-    public void AddNewObject()
+    private TextMeshProUGUI tmp;
+
+    public void AddNewObject(string type, string text)
     {
+        
         var newObj = Instantiate(uiPrefabs, scrollRect.content).GetComponent<RectTransform>();
+        
+        tmp = newObj.GetComponent<TextMeshProUGUI>();
+        
+
         uiObjects.Add(newObj);
+
+        tmp.alignment = TextType(type);
+        tmp.text = text;
+
+        Canvas.ForceUpdateCanvases();
 
         float y = 0f;
         for(int i = 0; i < uiObjects.Count; i++)
         {
+            Debug.Log(y);
             uiObjects[i].anchoredPosition = new Vector2(0f, -y);
-            y += uiObjects[i].sizeDelta.y + space;
+            y += uiObjects[i].rect.height;
         }
 
         scrollRect.content.sizeDelta = new Vector2 (scrollRect.content.sizeDelta.x, y);
     }
+
+    private TextAlignmentOptions TextType(string type)
+    {
+        switch (type)
+        {
+            case "top":
+                return TextAlignmentOptions.TopLeft;
+            case "bottom":
+                return TextAlignmentOptions.BottomLeft;
+            case "middle":
+                return TextAlignmentOptions.MidlineLeft;
+        }
+
+        return TextAlignmentOptions.TopLeft;
+    }
+
 }
--- EpPublic/_TextManager.cs
+++ EpPublic/_TextManager.cs
@@ -12,6 +12,7 @@ public class _TextManager : _LoadCSVTable
     public _ScrollerControll scrollerControll;
 
     private List<string> _eventList = new List<string>();
+    private List<string> _textTypeList = new List<string>();
     private List<string> _textList = new List<string>();
     private List<string> _selectList = new List<string>();
     private List<string> _selectCardList = new List<string>();
@@ -21,34 +22,44 @@ public class _TextManager : _LoadCSVTable
     private void Awake()
     {
         _eventList = GetEvent("10001", "Event");
+        _textTypeList = GetEvent("10001", "TextType");
         _textList = GetEvent("10001", "Text");
         _selectList = GetEvent("10001", "Select");
         _selectList = GetEvent("10001", "Select");
         _rewordList = GetEvent("10001", "Reword");
         _rewordTextList = GetEvent("10001", "RewordText");
 
-        foreach (var text in _eventList)
-            Debug.Log(text);
+        //foreach (var text in _eventList)
+        //    Debug.Log(text);
 
-        foreach (var text in _textList)
-            Debug.Log(text);
+        //foreach (var text in _textList)
+        //    Debug.Log(text);
 
-        foreach (var text in _selectList)
-            Debug.Log(text);
+        //foreach (var text in _selectList)
+        //    Debug.Log(text);
 
     }
 
     private void OnEnable()
     {
-        for(int i = 0; i < 5; i++)
-            PrintMainText();
+        PrintMainText(_textTypeList, _textList);
+
         PrintText(Select, _selectList);
     }
 
-    private void PrintMainText()
+    private void PrintMainText(List<string> type, List<string> list)
     {
-        scrollerControll.AddNewObject();
+        int index = 0;
 
+        foreach (var text in list)
+        {
+            if (text.Length < 1)
+                scrollerControll.AddNewObject(type[index], "\n\n");
+
+            scrollerControll.AddNewObject(type[index], text);
+            
+            index++;
+        }
     }
 
     private void PrintText(TextMeshProUGUI[] tmp, List<string> list)
@@ -57,10 +68,8 @@ public class _TextManager : _LoadCSVTable
 
         foreach (var text in list)
         {
-            if (text.Length < 1)
-                break;
-
-            tmp[index++].text = text;
+            if (text.Length > 0)
+                tmp[index++].text = text;
         }
     }
 }
